c206b0103eda299908eecd8aa0ab001944005e40
Pavel <dichenkopavel@gmail.com>
2022-01-14 00:49:17 +0300

    feat add ui-kit button

---
+++ src/ui-kit/Button/buttonVariants.js
@@ -0,0 +1,13 @@
+export default () => ({
+  primary: {
+    color: "textMain",
+    bg: "primaryYellow",
+  },
+
+  outlined: {
+    color: "#FFBC01",
+    bg: "backgroundMain",
+    border: "1px solid",
+    borderColor: "#FFBC01",
+  },
+});
