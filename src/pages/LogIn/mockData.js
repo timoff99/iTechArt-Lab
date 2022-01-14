f73831a87e274636277c57ab9e4389ef05638829
Pavel <pavel@wse-161-71b.icx.local>
2022-01-14 14:43:08 +0300

    add all

---
+++ src/pages/LogIn/mockData.js
@@ -0,0 +1,41 @@
+export const loginData = [
+  {
+    type: "text",
+    label: "Email",
+    name: "Email",
+  },
+  {
+    type: "password",
+    label: "Password",
+    name: "Password",
+  },
+
+  /////////delet me........
+  {
+    type: "text",
+    name: "smallSearch",
+  },
+  {
+    type: "text",
+    name: "bigSearch",
+  },
+  /////////delet me........
+];
+
+export const signUpData = [
+  {
+    type: "text",
+    label: "Email",
+    name: "Email",
+  },
+  {
+    type: "password",
+    label: "Password",
+    name: "Password",
+  },
+  {
+    type: "password",
+    label: "Confirm Password",
+    name: "Password",
+  },
+];
---
+++ src/ui-kit/Form/index.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+
+import { ReactComponent as Logo } from "../../static/logo.svg";
+import { Box } from "../Box";
+import { Button } from "../Button";
+import { Input } from "../Input";
+import { ReactComponent as Send } from "../../static/send.svg";
+
+const StyledSend = styled(Send)`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+const StyledForm = styled(Box)`
+  background: #cfc7c7;
+  width: 600px;
+  height: 654px;
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 15%);
+  margin: 100px;
+  border-radius: 40px 10px;
+  padding: 50px;
+`;
+
+export const Form = ({ title, description, inputData, buttonText }) => {
+  return (
+    <StyledForm>
+      <Logo />
+      <h3>{title}</h3>
+      <p>{description}</p>
+      {inputData.map((props) => {
+        return <Input {...props} />;
+      })}
+
+      <Button>{buttonText}</Button>
+    </StyledForm>
+  );
+};
